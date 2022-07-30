5b292291cb4a4264d92fd3ca043ac08089ae6597
smrTofighi <mrtofxn@gmail.com>
2022-07-30 18:13:38 +0430

    create models tag,poster and padcasts

---
+++ lib/models/padcats_model.dart
@@ -0,0 +1,27 @@
+class PadcastModel {
+  String? id;
+  String? title;
+  String? poster;
+  String? publisher;
+  String? view;
+  String? createdAt;
+  PadcastModel(
+    {
+      required this.id,
+      required this.title,
+      required this.poster,
+      required this.publisher,
+      required this.view,
+      required this.createdAt
+    }
+  );
+
+  PadcastModel.fromJson(Map<String,dynamic> element){
+    id = element['id'];
+    title = element['title'];
+    poster = element['poster'];
+    publisher = element['publisher'];
+    view = element['view'];
+    createdAt = element['created_at'];
+  }
+}
